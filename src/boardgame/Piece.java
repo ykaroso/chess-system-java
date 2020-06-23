f171ff1d02e6c7ba59e495b1a6a4d078a61630e6
Icaro Sidou <icaroso__@hotmail.com>
2020-06-23 16:14:39 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,19 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		System.out.println("Criando nova peça");
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
 
 }
